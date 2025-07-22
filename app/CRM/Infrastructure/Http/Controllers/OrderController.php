ec2d83d08f9acba05157cfc1ccaef95f9f3cc0e1
kovyakin <kdv-1974@mail.ru>
2025-07-22 22:50:48 +0400

    prepare 4

--- app/CRM/Infrastructure/Http/Controllers/OrderController.php
+++ app/CRM/Infrastructure/Http/Controllers/OrderController.php
@@ -2,26 +2,37 @@
 
 namespace App\CRM\Infrastructure\Http\Controllers;
 
+use App\CRM\Domain\Core\Contracts\OrderContract;
+use App\CRM\Infrastructure\Http\Requests\OrderCreateRequest;
 use App\CRM\Infrastructure\Http\Requests\OrderFilterRequest;
 use App\CRM\Infrastructure\Http\Resources\OrderFilteredResource;
 use App\Http\Controllers\Controller;
 
-
 class OrderController extends Controller
 {
     public function __construct(
-        private readonly OrderFilterRequest $orderFilterRequest
+        private readonly OrderFilterRequest $orderFilterRequest,
+        private readonly OrderContract $orderService
     ) {
     }
 
     /**
-     * Handle the incoming request.
+     * Display a listing of the resource.
      */
-    public function __invoke()
+    public function index()
     {
         //todo исправить пагинацию
         $orders = $this->orderFilterRequest->status('active')->paginate(12)->get();
 
         return OrderFilteredResource::collection($orders);
     }
+
+    public function create(OrderCreateRequest $request)
+    {
+        $dto = $request->getDTO();
+
+        $this->orderService->create($dto);
+    }
+
+
 }
